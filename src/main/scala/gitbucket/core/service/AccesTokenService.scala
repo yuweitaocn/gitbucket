0ef5d9146770c207b174a17426ee485be1e2d018
shimamoto <chibochibo@gmail.com>
2015-08-16 21:34:23 +0900

    Fix AccessTokenService.

--- src/main/scala/gitbucket/core/service/AccesTokenService.scala
+++ src/main/scala/gitbucket/core/service/AccesTokenService.scala
@@ -1,11 +1,13 @@
 package gitbucket.core.service
 
 import gitbucket.core.model.Profile._
-import profile.simple._
+import profile.api._
 
 import gitbucket.core.model.{Account, AccessToken}
 import gitbucket.core.util.StringUtil
+import slick.dbio.{FailureAction, SuccessAction}
 
+import scala.annotation.tailrec
 import scala.util.Random
 
 
@@ -22,32 +24,44 @@ trait AccessTokenService {
   /**
    * @retuen (TokenId, Token)
    */
-  def generateAccessToken(userName: String, note: String)(implicit s: Session): (Int, String) = {
-    var token: String = null
-    var hash: String = null
-    do{
-      token = makeAccessTokenString
-      hash = tokenToHash(token)
-    }while(AccessTokens.filter(_.tokenHash === hash.bind).exists.run)
-    val newToken = AccessToken(
-        userName = userName,
-        note = note,
-        tokenHash = hash)
-    val tokenId = (AccessTokens returning AccessTokens.map(_.accessTokenId)) += newToken
-    (tokenId, token)
+  def generateAccessToken(userName: String, note: String): DBIO[(Int, String)] = {
+
+    def hash0(token: String = makeAccessTokenString): DBIO[(String, String)] = {
+      val hash = tokenToHash(token)
+      for {
+        exists <- AccessTokens.filter(_.tokenHash === hash.bind).exists.result
+        if !exists
+      } yield
+        hash -> token
+    }
+
+    @tailrec
+    def generate0(): DBIO[(Int, String)] = {
+      hash0() match {
+        case SuccessAction((hash, token)) =>
+          ((AccessTokens returning AccessTokens.map(_.accessTokenId)) += AccessToken(
+            userName  = userName,
+            note      = note,
+            tokenHash = hash)) map (_ -> token)
+        case FailureAction(_) => generate0()
+      }
+    }
+
+    generate0()
   }
 
-  def getAccountByAccessToken(token: String)(implicit s: Session): Option[Account] =
+  def getAccountByAccessToken(token: String): DBIO[Option[Account]] =
     Accounts
-      .innerJoin(AccessTokens)
+      .join(AccessTokens)
       .filter{ case (ac, t) => (ac.userName === t.userName) && (t.tokenHash === tokenToHash(token).bind) && (ac.removed === false.bind) }
       .map{ case (ac, t) => ac }
-      .firstOption
+      .result
+      .headOption
 
-  def getAccessTokens(userName: String)(implicit s: Session): List[AccessToken] =
-    AccessTokens.filter(_.userName === userName.bind).sortBy(_.accessTokenId.desc).list
+  def getAccessTokens(userName: String): DBIO[Seq[AccessToken]] =
+    AccessTokens.filter(_.userName === userName.bind).sortBy(_.accessTokenId.desc).result
 
-  def deleteAccessToken(userName: String, accessTokenId: Int)(implicit s: Session): Unit =
+  def deleteAccessToken(userName: String, accessTokenId: Int): DBIO[Int] =
     AccessTokens filter (t => t.userName === userName.bind && t.accessTokenId === accessTokenId) delete
 
 }
